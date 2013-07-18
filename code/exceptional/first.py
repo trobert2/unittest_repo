784c5143d02e38ab639bed53aa5be709666e4314
Robert Tingirica <rtingirica@cloudbasesolutions.com>
2013-07-18 17:12:39 +0300

    second test is prepared (test exceptions)

--- code/exceptional/first.py
+++ code/exceptional/first.py
@@ -2,11 +2,21 @@
 
 import urllib2
 
+class DataNotFound(Exception):
+    pass
+
 class First(object):
     def get_data(self, path):
 
         req = urllib2.Request(path)
-        response = self.urllib2.urlopen(req)
+        try:
+            response = self.urllib2.urlopen(req)
+        except urllib2.HTTPError() as ex:
+                if ex.code == 404:
+                    raise DataNotFound()
+                else:
+                    raise
+
         return response.read()
 
     def post_data(self, destination, data):
