87bd92d4deeb24667ce014d314f9750f129e3c74
Robert Tingirica <rtingirica@cloudbasesolutions.com>
2014-03-06 15:28:06 +0200

    Teste Denis

--- test_second.py
+++ test_second.py
@@ -1,36 +1,15 @@
-
+import mock
 import unittest
-import mox
-
-import first
 import second
 
-
-class FirstTest(unittest.TestCase):
-
+class TestSecond(unittest.TestCase):
     def setUp(self):
-        self.fake_location = 'http://fake.example.example.net.org.com'
-        self.fake_destination = 'http://fake.com/post_location'
-        self.fake_data = 'fake_data'
-
         self.obj = second.Second()
 
-        self.mox = mox.Mox()
-        self.mox.StubOutWithMock(first.First, 'get_data')
-        self.mox.StubOutWithMock(first.First, 'post_data')
-
-    def tearDown(self):
-        self.mox.UnsetStubs()
-
-    def test_post_and_get(self):
-        m = first.First.get_data(self.fake_location)
-        m.AndReturn(self.fake_data)
-
-        first.First.post_data(self.fake_destination, self.fake_data)
-
-        self.mox.ReplayAll()
-        response = self.obj.get_and_post(self.fake_location,
-                                         self.fake_destination)
-        self.mox.VerifyAll()
-
-        self.assertEqual(response, None)
+    @mock.patch('first.First.get_data')
+    @mock.patch('first.First.post_data')
+    def test_get_and_post(self, mock_post_data, mock_get_data):
+        mock_get_data.return_value = 'data'
+        self.obj.get_and_post("loc", "dest")
+        mock_get_data.assert_called_with('loc')
+        mock_post_data.assert_called_with('dest', 'data')
